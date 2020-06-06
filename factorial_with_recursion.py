bd9a048ca2aedc4e2f332eeadc313ea609c66516
haliluluoglu <haliluluoglu@gmail.com>
2020-06-06 18:19:20 +0300

    Factorial finding with recursive algorithm in Python

---
+++ factorial_with_recursion.py
@@ -0,0 +1,9 @@
+
+def recursive_factorial(n):
+    if n==1:
+        return 1
+    else:
+        return n*recursive_factorial(n-1)
+
+factorial=int(input("PLease enter a number which you want to calculate factorial: "))
+print(recursive_factorial(factorial))
