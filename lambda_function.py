7558b1c68d03f00b081b91020ba8612449ec48ed
haliluluoglu <haliluluoglu@gmail.com>
2020-06-07 12:35:19 +0300

    Using lambda function examples with filter(), map(), reduce() functions

---
+++ lambda_function.py
@@ -0,0 +1,19 @@
+from functools import reduce
+
+numbers=[0,1,2,3,4,5,6,7,8,9]
+
+evens=list(filter(lambda e: e%2==0, numbers))
+print(evens)
+odds=list(filter(lambda o: o%2==1, numbers))
+print(odds)
+
+double_evens=list(map(lambda de: de*2, evens))
+print(double_evens)
+double_odds=list(map(lambda do: do*2, odds))
+print(double_odds)
+
+sum_evens=reduce(lambda a,b: a+b, evens)
+sum_odds=reduce(lambda a,b: a+b, odds)
+print("{} sum of even numbers".format(sum_evens))
+print("{} sum of odd numbers".format(sum_odds))
+
