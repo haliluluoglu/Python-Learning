510f866741949c45989cc2e93a63c2d0a74eeba0
haliluluoglu <haliluluoglu@gmail.com>
2020-06-10 13:08:57 +0300

    Create a simple inner class in Python.

---
+++ method_resolution_order.py
@@ -0,0 +1,20 @@
+class A:
+    def __init__(self):
+        print("Class A init function")
+
+    def feature1(self):
+        print("Class A feature1 function")
+
+class B:
+    def __init__(self):
+        print("Class B init function")
+    def feature1(self):
+        print("Class B feature1 function")
+
+class C(A,B):
+    def __init__(self):
+        super().__init__()
+        print("Class C init function")
+
+class_c_object=C()
+
